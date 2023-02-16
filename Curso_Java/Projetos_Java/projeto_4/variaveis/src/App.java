6d71928c1fe4b1b6f44f688d33c181e65d778cba
Vinicius Salles - YoungBR <106876892+youngbr87@users.noreply.github.com>
2023-02-16 15:15:22 -0300

    1.1

---
+++ Curso_Java/Projetos_Java/projeto_4/variaveis/src/App.java
@@ -0,0 +1,40 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+        //variaveis do tipo inteiro
+        
+        byte numeroPequeno = 125;
+        short numeroShort = 32565;
+        int numeroInt = 768886763;
+        long numeroLong = 3572198327887656787l;
+
+        //variaveis do tipo Ponto fluante
+
+        float peso = 78.7f;
+        double pi = 3.14737373773262662626262;
+
+        //varuaveus do tipo caracter
+
+        char letra = 'c';
+
+
+        System.out.println("Variaveis do tipo inteiro inteiro");
+        System.out.println(" ");
+        System.out.println(numeroPequeno);
+        System.out.println(numeroShort);
+        System.out.println(numeroInt);
+        System.out.println(numeroLong);
+        System.out.println(" ");
+
+        System.out.println("Variaveis do tipo inteiro flutuante");
+        System.out.println(peso);
+        System.out.println(pi);
+        System.out.println(" ");
+        
+        System.out.println("Variaveis do tipo inteiro caractere");
+        System.out.println(letra);
+
+
+
+    }
+}

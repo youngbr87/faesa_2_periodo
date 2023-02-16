38d081537a9e877167eebb4aca222c1d22778c0c
Vinicius Salles - YoungBR <106876892+youngbr87@users.noreply.github.com>
2023-02-16 14:39:29 -0300

    1.0

---
+++ Curso_Java/Projetos_Java/projeto_2/projeto2.java
@@ -0,0 +1,9 @@
+/**
+ * projeto2
+ */
+public class projeto2 {
+
+    public static void main(String[] args) {
+        System.out.println("Escrevendo em JAVA");
+    }
+}
---
+++ Curso_Java/Projetos_Java/projeto_3/projeto/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Configurando workspace!");
+        System.out.println("Imprimindo mais coisas");
+    }
+}

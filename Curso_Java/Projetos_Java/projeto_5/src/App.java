a858ac13e52d05e0ce2aefa17c2f4a97d2d302f3
Vinicius Salles - YoungBR <106876892+youngbr87@users.noreply.github.com>
2023-03-02 16:51:27 -0300

    1.1

---
+++ Curso_Java/Projetos_Java/projeto_5/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+    
+        String filme = "Top Gun Maverick";
+        int anoLancamento = 2022;
+        int duracao = 274;
+        float notaCritica = 8.2f;
+        char letraInicial = 'T';
+        
+
+
+
+
+    }
+}

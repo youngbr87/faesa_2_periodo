51cacc21beab8133ad9faf6289996a0cbc78256b
Vinicius Salles - YoungBR <106876892+youngbr87@users.noreply.github.com>
2023-03-09 20:01:03 -0300

    1.1.1

--- LP_2/exerecicio_lp2_1/src/exerecicio_lp2_1/exerecicio_lp2_1.java
+++ LP_2/exerecicio_lp2_1/src/exerecicio_lp2_1/exerecicio_lp2_1.java
@@ -8,6 +8,15 @@ public class exerecicio_lp2_1 {
 		System.out.println("X   X"); 
 		System.out.println("X   X"); 
 		System.out.println("XXXXX");
+		     
+		/*usando tabulação */
+       
+                System.out.println("ALUNO (A)\t\tNOTA ");
+		System.out.println("=========\t\t===== ");
+		System.out.println($al1 + "\t\t\t"+$not1);
+		System.out.println($al2 + "\t\t\t"+$not2);
+		System.out.println($al3 + "\t\t\t"+$not3);
+		System.out.println($al4 + "\t\t\t"+$not4);
 	
 
 

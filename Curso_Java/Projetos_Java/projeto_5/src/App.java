4a4d85a5e651b096bba680ff40e41259d4f32e68
Vinicius Salles - YoungBR <106876892+youngbr87@users.noreply.github.com>
2023-03-15 08:48:08 -0300

    1.1.1

--- Curso_Java/Projetos_Java/projeto_5/src/App.java
+++ Curso_Java/Projetos_Java/projeto_5/src/App.java
@@ -1,17 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
     
-        String filme = "Top Gun Maverick";
-        int anoLancamento = 2022;
-        int duracao = 274;
-        float notaCritica = 8.2f;
-        char letraInicial = 'T';
+        String $filme = "Top Gun Maverick";
+        int $anoLancamento = 2022;
+        int $duracao = 274;
+        float $notaCritica = 8.2f;
+        String $letraInicial = "T";
 
-        System.out.printf("Título: %s\n", filme);
-        System.out.printf("Ano de lançamento: %d\n", anoLancamento);
-        System.out.printf("Duração: %s\n","\f minutos");
-        System.out.println(notaCritica);
-        System.out.println(letraInicial);
+        System.out.printf("Título: %s\n", $filme);
+        System.out.printf("Ano de lançamen4to: %d\n", $anoLancamento);
+        System.out.printf("Duração: %s\n", $duracao + "\f minutos");
+        System.out.printf("Nota crítica: %.1f\n", $notaCritica);
+        System.out.printf("Letra inicial: %s\n", $letraInicial);
 
 
 
